9a39bd3638ac181af6801ac27fe425ceca89ca30
Tushar Mittal <tushar.mittal@qlik.com>
2025-06-11 16:47:52 +0530

    unused code removed from tap_lever

--- tap_lever/__init__.py
+++ tap_lever/__init__.py
@@ -21,34 +21,6 @@ class LeverRunner:
         self.client = client
         self.available_streams = available_streams
 
-    def get_streams_to_replicate(self):
-        streams = []
-
-        if not self.catalog:
-            return streams
-
-        for stream_catalog in self.catalog.streams:
-            if not is_stream_selected(stream_catalog):
-                LOGGER.info("'{}' is not marked selected, skipping."
-                            .format(stream_catalog.stream))
-                continue
-
-            for available_stream in self.available_streams:
-                if available_stream.matches_catalog(stream_catalog):
-                    if not available_stream.requirements_met(self.catalog):
-                        raise RuntimeError(
-                            "{} requires that that the following are "
-                            "selected: {}"
-                            .format(stream_catalog.stream,
-                                    ','.join(available_stream.REQUIRES)))
-
-                    to_add = available_stream(
-                        self.config, self.state, stream_catalog, self.client)
-
-                    streams.append(to_add)
-
-        return streams
-
     def do_discover(self):
         LOGGER.info("Starting discovery.")
 
@@ -60,28 +32,6 @@ class LeverRunner:
             catalog += stream.generate_catalog()
 
         json.dump({'streams': catalog}, sys.stdout, indent=4)
-
-    def do_sync(self):
-        LOGGER.info("Starting sync.")
-
-        streams = self.get_streams_to_replicate()
-
-        for stream in streams:
-            try:
-                stream.state = self.state
-                stream.sync()
-                self.state = stream.state
-            except OSError as e:
-                LOGGER.error(str(e))
-                exit(e.errno)
-
-            except Exception as e:
-                LOGGER.error(str(e))
-                LOGGER.error('Failed to sync endpoint {}, moving on!'
-                             .format(stream.TABLE))
-                raise e
-
-        save_state(self.state)
 
     def get_streams_to_replicate(self):
         streams = []
